6f0f0af7149dfc03d080a263821dea3c26c43245
churuxu <churuxu@126.com>
2019-07-26 09:55:54 +0800

    update support sub cmake

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "crc16.h"
 #include "zlib.h"
 #include "json.h"
+#include "duktape.h"
 
 int main(){
 	uint16_t crc = crc16(0, (uint8_t*)"123456", 6);
@@ -12,7 +13,12 @@ int main(){
 	printf("crc32=%08x\n", crc32);
 	auto json = json_parse("{\"id\":123}", 3);
 	printf("%d\n", (int)(*json)["id"]);
+
+	auto duk = duk_create_heap_default();
+	duk_eval_string(duk, "1+2");
+	printf("%d\n", duk_to_int(duk, -1));
 	return 0;
+	
 }
 
 
